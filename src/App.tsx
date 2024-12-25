749f0f9722be435c978771399d531262a33a82b8
Luisa Reis <git@luisareis.com>
2024-12-24 18:41:43 -0800

    Add play toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import {createSignal, type Component} from 'solid-js';
+import Fa from 'solid-fa';
+import {faPlay, faStop} from '@fortawesome/free-solid-svg-icons';
 
 import ThemeToggle from './ThemeToggle';
 
 const App: Component = () => {
   const [frequency, setFrequency] = createSignal<number | null>(440);
+  const [playing, setPlaying] = createSignal<boolean>(false);
 
   return (
     <>
@@ -15,7 +18,7 @@ const App: Component = () => {
           <ThemeToggle />
         </div>
       </div>
-      <div class="container mx-auto card card-compact bg-base-100 w-3/4 shadow-xl">
+      <div class="container mx-auto card card-compact bg-base-100 w-1/4 shadow-xl">
         <div class="card-body">
           <label class="form-control w-full max-w-xs">
             <div class="label">
@@ -35,6 +38,17 @@ const App: Component = () => {
               }}
             />
           </label>
+          <div class="form-control">
+            <label class="label cursor-pointer">
+              <span class="label-text">Play</span>
+              <input
+                type="checkbox"
+                class="toggle"
+                checked={playing()}
+                onInput={(e) => setPlaying(e.currentTarget.value === 'on')}
+              />
+            </label>
+          </div>
         </div>
       </div>
     </>
