1ccb86479089915f9b5798423368918de143ee3d
Luisa Reis <git@luisareis.com>
2024-12-24 17:35:41 -0800

    Add Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import solid from "eslint-plugin-solid";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import solid from 'eslint-plugin-solid';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist/*"] },
-  { files: ["**/*.{js,mjs,cjs,ts,tsx}"] },
+  {ignores: ['dist/*']},
+  {files: ['**/*.{js,mjs,cjs,ts,tsx}']},
   {
-    files: ["**/*.js"],
-    languageOptions: { sourceType: "commonjs" },
-    plugins: { solid },
+    files: ['**/*.js'],
+    languageOptions: {sourceType: 'commonjs'},
+    plugins: {solid},
   },
-  { languageOptions: { globals: globals.browser } },
+  {languageOptions: {globals: globals.browser}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
